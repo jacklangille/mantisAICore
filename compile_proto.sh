bafb5994551faae24e3e6e1e9f3c0e402ee685fa
Jack Langille <jwl@macbook-pro.home>
2024-09-03 19:37:10 -0300

    fixes

--- compile_proto.sh
+++ compile_proto.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-PROTO_FILE="../proto/protocol.proto"
-OUT_DIR="../proto/"
+PROTO_FILE="proto/protocol.proto"
+OUT_DIR="proto/"
 
 
 protoc --cpp_out=${OUT_DIR} --proto_path=${OUT_DIR} ${PROTO_FILE}
